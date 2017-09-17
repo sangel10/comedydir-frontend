173d21cfa4420597b0f5d2ec64736f68a662f8b8
Santiago Angel <santiagoangel10@gmail.com>
2017-09-17 07:34:18 +0200

    Remove starter components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React from 'react' // eslint-disable-line no-unused-vars
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 import Home from './components/Home'
-import About from './components/About'
-import Topics from './components/Topics'
 import Country from './components/Country'
 import './App.css'
 
@@ -12,16 +10,13 @@ const BasicExample = () => (
       <ul>
         <li><Link to="/">Home</Link></li>
         <li><Link to="/country">Country</Link></li>
-        <li><Link to="/about">About</Link></li>
-        <li><Link to="/topics">Topics</Link></li>
       </ul>
 
       <hr/>
 
       <Route exact path="/" component={Home}/>
       <Route path="/country" component={Country}/>
-      <Route path="/about" component={About}/>
-      <Route path="/topics" component={Topics}/>
+
     </div>
   </Router>
 )
